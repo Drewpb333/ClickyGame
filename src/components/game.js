cb5cc8eb93fc3bef8c76b0ffa4c4fa80f2c9a10e
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-05-14 19:58:25 -0400

    changes to shufflephotos method

--- src/components/game.js
+++ src/components/game.js
@@ -1,20 +1,55 @@
 import React, {Component} from 'react';
 import Header from './header';
+import franklin from '../../public/img/franklin.jpg';
+import linus from '../../public/img/linus.jpg';
+import lucy from '../../public/img/lucy.jpg';
+import marcy from '../../public/img/marcy.jpg';
+import olaf from '../../public/img/olaf.jpg';
+import peppermintPatty from '../../public/img/peppermint-patty.jpg';
+import pigpen from '../../public/img/pigpen.jpg';
+import rerun from '../../public/img/rerun.jpg';
+import sally from '../../public/img/sally.jpg';
+import schroeder from '../../public/img/schroeder.jpg';
+import snoopy from '../../public/img/snoopy.jpg';
+import woodstock from '../../public/img/woodstock.jpg';
+
+const photosArr = [franklin, linus, lucy, marcy, olaf, peppermintPatty, pigpen, rerun, sally, schroeder, snoopy, woodstock];
 
 class Game extends Component {
     state = {
         score: 0,
-        highestScore: 0
+        highestScore: 0,
+        photos: []
     };
 
-    shufflePhotos = () => ;
-    //use fisher-yates shuffle here
+    //fisher-yates shuffle
+    shufflePhotos = arr => {
+        let currentIndex = arr.length, temporaryValue, randomIndex;
     
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+    
+            // picks a remaining element
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-    componentDidMount () {
-        this.shufflePhotos();
+            // And swaps it with another element
+            temporaryValue = arr[currentIndex];
+            arr[currentIndex] = arr[randomIndex];
+            arr[randomIndex] = temporaryValue;
+        };
+        this.setState({photos: arr});
     }
 
+    
+
+    componentDidMount() {
+        this.shufflePhotos(photosArr);
+    }
+
+    displayPhotos = arr => (
+
+    )
 
     alreadyClicked = hasBeenClicked => (
         if(hasBeenClicked && this.score > this.highestScore){
@@ -30,44 +65,45 @@ class Game extends Component {
     )
 
     render() {
+        return(
         <div className="peanuts-images">
             <div className="row">
                 <div className="col-2-xs"></div>
+                <div className="col-2-xs" hasBeenClicked={false} id={photos[0]}></div>
+                <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[1]}></div>
                 <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[2]}></div>
-                <div className="col-1-xs"></div>
-                <div className="col-2-xs" hasBeenClicked={false} id={photos[3]}></div>
                 <div className="col-2-xs"></div>
             </div>
             <div className="row">
                 <div className="col-2-xs"></div>
+                <div className="col-2-xs" hasBeenClicked={false} id={photos[3]}></div>
+                <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[4]}></div>
                 <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[5]}></div>
-                <div className="col-1-xs"></div>
-                <div className="col-2-xs" hasBeenClicked={false} id={photos[6]}></div>
                 <div className="col-2-xs"></div>
             </div>
             <div className="row">
                 <div className="col-2-xs"></div>
+                <div className="col-2-xs" hasBeenClicked={false} id={photos[6]}></div>
+                <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[7]}></div>
                 <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[8]}></div>
-                <div className="col-1-xs"></div>
-                <div className="col-2-xs" hasBeenClicked={false} id={photos[9]}></div>
                 <div className="col-2-xs"></div>
             </div>
             <div className="row">
                 <div className="col-2-xs"></div>
+                <div className="col-2-xs" hasBeenClicked={false} id={photos[9]}></div>
+                <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[10]}></div>
                 <div className="col-1-xs"></div>
                 <div className="col-2-xs" hasBeenClicked={false} id={photos[11]}></div>
-                <div className="col-1-xs"></div>
-                <div className="col-2-xs" hasBeenClicked={false} id={photos[12]}></div>
                 <div className="col-2-xs"></div>
             </div>
-        </div>
+        </div>)
     };
 }
 
